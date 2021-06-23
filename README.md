a090715984255e3ed62c8ef3f4e33772de2548d2
Paulo Augusto <pademacena@gmail.com>
2021-06-23 16:45:30 -0300

    Terceira aula, Middleware de error separado

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 
 - Cadastrar tag
 
-  - [ ] Não é permitido cadastrar mais de uma tag com o mesmo nome 
+  - [x] Não é permitido cadastrar mais de uma tag com o mesmo nome 
 
-  - [ ] Não é permitido cadastrar tag sem nome 
+  - [x] Não é permitido cadastrar tag sem nome 
 
-  - [ ] Não é permitifo o cadastro por usuário que não seja adm
+  - [x] Não é permitido o cadastro por usuário que não seja adm
 
 - Cadastro de Elogios
 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'reflect-metadata';
+import "express-async-errors"
 
 import { router } from "./routes"
 
 import './database';
+import { errorHandling } from './middleware/errorHandling';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(errorHandling)
 
 app.listen(3000, () => {
   console.log("server is running in 3000 port");
